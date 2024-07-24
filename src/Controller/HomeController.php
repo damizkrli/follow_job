01b9d71e7ad292cf3cba4bc0d217440f9414ce7f
Damien Carli <karli.damien@gmail.com>
2024-07-24 17:03:02 +0200

    Slugify reoute HomeController => follow-job-homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use Cocur\Slugify\SlugifyInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    private SlugifyInterface $slugify;
+
+    public function __construct(SlugifyInterface $slugify)
     {
+        $this->slugify = $slugify;
+    }
+
+    #[Route('/{title}', name: 'home')]
+    public function index(Request $request, string $title): Response
+    {
+        $expectedSlug = 'follow-job-homepage';
+
+        if ($this->slugify->slugify($title) !== $expectedSlug) {
+            return new RedirectResponse($this->generateUrl('home', ['title' => $expectedSlug]));
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'slug' => $expectedSlug
         ]);
     }
 }
