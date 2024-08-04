94e05f53b35ee4552c2dff9367de63f2f7eb5e23
Damien Carli <karli.damien@gmail.com>
2024-08-04 19:34:28 +0200

    Ajout des assert, ajout des validators et ajout de UniqueEntity

--- src/Entity/JobBoard.php
+++ src/Entity/JobBoard.php
@@ -5,8 +5,16 @@ namespace App\Entity;
 use App\Repository\JobBoardRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Validator\JobBoard as HasUniqueJobboard;
+use App\Validator\JobBoard as HasSpacesInJobboardName;
 
 #[ORM\Entity(repositoryClass: JobBoardRepository::class)]
+#[UniqueEntity(
+    fields: ['name'],
+    message: '',
+)]
 class JobBoard
 {
     #[ORM\Id]
@@ -15,12 +23,38 @@ class JobBoard
     private ?int $id = null;
 
     #[ORM\Column(length: 25)]
+    #[HasUniqueJobboard\HasUniqueJobboard()]
+    #[HasSpacesInJobboardName\HasSpacesInJobboardName()]
+    #[Assert\NotBlank(
+        message: 'Cette valeur ne doit pas être vide',
+    )]
+    #[Assert\Length(
+        min: 2,
+        max: 255,
+        minMessage: 'Cette valeur devrait comporter {{ limit }} caractères ou plus.',
+        maxMessage: 'Cette valeur devrait comporter {{ limit }} caractères ou moins.'
+    )]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(
+        message: 'Cette valeur ne doit pas être vide'
+    )]
+    #[Assert\Url(
+        message: 'Cette valeur n\'est pas une URL valide'
+    )]
+    #[Assert\Length(
+        min: '10',
+        max: '50',
+        minMessage: 'Cette valeur devrait comporter {{ limit }} caractères ou plus.',
+        maxMessage: 'Cette valeur devrait comporter {{ limit }} caractères ou moins.',
+    )]
     private ?string $link = null;
 
     #[ORM\Column(nullable: true)]
+    #[Assert\NotNull(
+        message: 'Cette valeur ne doit pas être nulle.'
+    )]
     private ?bool $profile = null;
 
     public function __toString(): string
@@ -40,7 +74,7 @@ class JobBoard
 
     public function setName(string $name): static
     {
-        $this->name = $name;
+        $this->name = trim(ucwords($name));
 
         return $this;
     }
@@ -52,7 +86,7 @@ class JobBoard
 
     public function setLink(string $link): static
     {
-        $this->link = $link;
+        $this->link = trim(strtolower($link));
 
         return $this;
     }
