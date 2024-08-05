61b0680d27ebdf7c1e01e5a85a8282060c29b71d
Damien Carli <karli.damien@gmail.com>
2024-08-05 12:19:40 +0200

    Ajout des fields dans le FormBuilder

--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,56 @@ class CompanyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('activity')
-            ->add('address')
-            ->add('postalCode')
-            ->add('city')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'entreprise',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Nom de l\'entreprise',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ]
+            ])
+            ->add('activity', TextType::class, [
+                'label' => 'Secteur d\'activité',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Agroalimentaire, Banque/Assurance, ...'
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => '10 rue de l\'Église',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('postalCode', IntegerType::class, [
+                'label' => 'Code postal',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => '75000'
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Ville',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
         ;
     }
 
@@ -24,6 +71,7 @@ class CompanyType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Company::class,
+            'validation_groups' => ['Default', 'unique_email'],
         ]);
     }
 }
