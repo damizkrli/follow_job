420888788e2a91d9c38394d87cc80867fd77f648
Damien Carli <karli.damien@gmail.com>
2024-08-09 16:09:49 +0200

    Création du validator pour tester l'unicité d'une candidature

---
+++ src/Validator/Application/HasUniqueApplicationPhp.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Validator\Application;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+class HasUniqueApplicationPhp extends Constraint
+{
+    public string $message = 'Vous avez déjà postulé à cette annonce.';
+}
---
+++ src/Validator/Application/HasUniqueApplicationPhpValidator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Validator\Application;
+
+use App\Repository\ApplicationRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class HasUniqueApplicationPhpValidator extends ConstraintValidator
+{
+
+    private ApplicationRepository $applicationRepository;
+
+    public function __construct(ApplicationRepository $applicationRepository)
+    {
+        $this->applicationRepository = $applicationRepository;
+    }
+
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        /* @var HasUniqueApplicationPhp $constraint */
+
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        $existingApplication = $this->applicationRepository->findOneBy([
+            'link' => $value
+        ]);
+
+        if(!$existingApplication) {
+            return;
+        }
+
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ value }}', $value)
+            ->addViolation();
+    }
+}
