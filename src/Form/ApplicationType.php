028ad34a334e34140ab30898929e845cc5a3e603
Damien Carli <karli.damien@gmail.com>
2024-08-09 12:40:38 +0200

    Mise à jour FormType

--- src/Form/ApplicationType.php
+++ src/Form/ApplicationType.php
@@ -6,10 +6,16 @@ use App\Entity\Application;
 use App\Entity\Company;
 use App\Entity\Contact;
 use App\Entity\JobBoard;
+use App\Repository\CompanyRepository;
+use App\Repository\ContactRepository;
+use App\Repository\JobBoardRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -25,36 +31,98 @@ class ApplicationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('job_title')
-            ->add('sent', null, [
-                'widget' => 'single_text',
+            ->add('statut', ChoiceType::class, [
+                'choices'    => self::STATUT,
+                'label'      => "Choisissez un statut",
+                'required'   => false,
+                'empty_data' => 'Envoyée',
+                'row_attr'   => [
+                    'class' => 'form-floating',
+                ]
             ])
-            ->add('response', null, [
-                'widget' => 'single_text',
+            ->add('job_title', TextType::class, [
+                'label'    => 'Intitulé du poste',
+                'required' => true,
+                'attr'     => [
+                    'placeholder' => 'Développeur Web Fullstack'
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ]
             ])
-            ->add('link', TextType::class, [
-                'label' => 'Lien'
+            ->add('sent', DateType::class, [
+                'widget'   => 'single_text',
+                'label'    => 'Date d\'envoi',
+                'required' => true,
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ]
             ])
-            ->add('note')
+            ->add('response', DateType::class, [
+                'widget'   => 'single_text',
+                'label'    => 'Date de réponse',
+                'required' => false,
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ]
+            ])
+            ->add('link', UrlType::class, [
+                'label'    => 'Lien vers l\'annonce',
+                'required' => true,
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+                'attr'     => [
+                    'placeholder' => 'Lien vers l\'annonce',
+                ]
+            ])
+            ->add('note', TextareaType::class, [
+                'label'    => 'Informations diverses',
+                'required' => false,
+                'attr'     => [
+                    'placeholder' => 'Date de création de l\'entreprise, valeurs de l\'entreprise, ...',
+                    'rows'        => '16',
+                    'cols'        => '20',
+                ],
+            ])
             ->add('company', EntityType::class, [
-                'class' => Company::class,
-                'choice_label' => 'name',
-                'mapped' => false,
+                'class'         => Company::class,
+                'choice_label'  => 'name',
+                'row_attr'      => [
+                    'class' => 'form-floating',
+                ],
+                'query_builder' => function (CompanyRepository $companyRepository) {
+                    return $companyRepository
+                        ->createQueryBuilder('company')
+                        ->orderBy('company.name', 'ASC');
+                },
+                'required'      => true,
             ])
             ->add('contact', EntityType::class, [
-                'class'        => Contact::class,
-                'choice_label' => 'firstname',
+                'class'         => Contact::class,
+                'choice_label'  => 'firstname',
+                'row_attr'      => [
+                    'class' => 'form-floating',
+                ],
+                'query_builder' => function (ContactRepository $contactRepository) {
+                    return $contactRepository
+                        ->createQueryBuilder('contact')
+                        ->orderBy('contact.firstname', 'ASC');
+                },
+                'required'      => true,
             ])
             ->add('job_board', EntityType::class, [
-                'class'        => JobBoard::class,
-                'choice_label' => 'name',
-            ])
-            ->add('statut', ChoiceType::class, [
-                'choices'     => self::STATUT,
-                'placeholder' => 'Choisissez un statut',
-                'label'       => "Statut",
-                'required'    => false,
-                'empty_data'  => 'Envoyée',
+                'class'         => JobBoard::class,
+                'choice_label'  => 'name',
+                'row_attr'      => [
+                    'class' => 'form-floating',
+                ],
+                'query_builder' => function (JobBoardRepository $jobBoardRepository) {
+                    return $jobBoardRepository
+                        ->createQueryBuilder('jobboard')
+                        ->orderBy('jobboard.name', 'ASC');
+                },
+                'required'      => true,
             ]);
     }
 
