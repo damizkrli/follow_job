1c8d8ae4e601a59d7c562c32346a5713338eeb3c
Damien Carli <karli.damien@gmail.com>
2024-08-13 20:48:34 +0200

    Changement de langue, maj home

---
+++ migrations/Version20240813094956.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240813094956 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE company CHANGE address address VARCHAR(75) DEFAULT NULL, CHANGE postal_code postal_code INT DEFAULT NULL, CHANGE city city VARCHAR(75) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE company CHANGE address address VARCHAR(75) NOT NULL, CHANGE postal_code postal_code INT NOT NULL, CHANGE city city VARCHAR(75) NOT NULL');
+    }
+}
