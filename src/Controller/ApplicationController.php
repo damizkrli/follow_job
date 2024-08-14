0d7badc853de33fd96c19b26fb5f7e7509397c41
Damien Carli <karli.damien@gmail.com>
2024-08-14 12:26:33 +0200

    Mise à jour des routes ApplicationController

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/candidature')]
+#[Route('/candidatures')]
 class ApplicationController extends AbstractController
 {
     #[Route('/', name: 'app_application_index', methods: ['GET'])]
@@ -42,7 +42,7 @@ class ApplicationController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_application_show', methods: ['GET'])]
+    #[Route('/details/{id}', name: 'app_application_show', methods: ['GET'])]
     public function show(Application $application): Response
     {
         return $this->render('application/show.html.twig', [
