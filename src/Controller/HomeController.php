04d4ece51378ca8e7f0d82115c8c89c57f09a85c
damizkrli <karli.damien@gmail.com>
2025-05-28 19:38:30 +0200

    ajout la vue count, ajout de externalLink

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\ExternalLink;
+use App\Form\ExternalLinkType;
+use App\Repository\ExternalLinkRepository;
+use App\Service\JobOfferApiService;
 use Cocur\Slugify\SlugifyInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\JobOfferApiService;
-use App\Service\RemoteOkJobService;
 
 class HomeController extends AbstractController
 {
@@ -21,12 +24,30 @@ class HomeController extends AbstractController
     }
 
     #[Route('/{title}', name: 'home')]
-    public function index(Request $request, string $title, JobOfferApiService $jobService): Response
-    {
+    public function index(
+        Request $request,
+        string $title,
+        JobOfferApiService $jobService,
+        ExternalLinkRepository $externalLinkRepository,
+        EntityManagerInterface $em
+    ): Response {
         $expectedSlug = 'follow-job-homepage';
 
         if ($this->slugify->slugify($title) !== $expectedSlug) {
             return new RedirectResponse($this->generateUrl('home', ['title' => $expectedSlug]));
+        }
+
+        $externalLinks = $externalLinkRepository->findAll();
+
+        $externalLink = new ExternalLink();
+        $form = $this->createForm(ExternalLinkType::class, $externalLink);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($externalLink);
+            $em->flush();
+            $this->addFlash('success', 'Lien ajouté avec succès.');
+            return $this->redirectToRoute('home', ['title' => $expectedSlug]);
         }
 
         $offers = $jobService->fetchOffers();
@@ -35,8 +56,8 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
             'slug' => $expectedSlug,
             'offers' => $offers,
+            'external_links' => $externalLinks,
+            'form' => $form->createView(),
         ]);
     }
-
-
 }
